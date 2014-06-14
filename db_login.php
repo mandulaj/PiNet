9ab7dd4625df0f964bc4b2331890f36b1b88f4b1
Jakub Mandula <jakub.aludnam@gmail.com>
2014-06-14 15:19:08 +0200

    Update db_login.php

--- db_login.php
+++ db_login.php
@@ -1,7 +1,7 @@
 <?php
 // This stores the user-name and password of the database!!!!!
 $USERNAME_db = "PiNet";
-$PASSWORD_db = "dh9q9j4eBCtsstpe";
+$PASSWORD_db = "dh9q9j4eBCtsstpe"; // Yes I changed it
 $HOST_db = "localhost";
 
 ?>
