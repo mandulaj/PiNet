432394f4d4b2fc8382d9f125b3efe960ba0628c6
Jakub Mandula <jakub.aludnam@gmail.com>
2014-10-11 17:54:57 +0200

    add routing paths!!

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express     = require('express'),
+  path          = require('path'),
+  favicon       = require('serve-favicon'),
+  logger        = require('morgan'),
+  cookieParser  = require('cookie-parser'),
+  bodyParser    = require('body-parser'),
+  passport      = require('passport'),
+  session       = require('express-session');
 
 var app = express();
 
@@ -20,10 +19,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "pianocat"
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+require('./routes/index')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,64 @@
+var jwt = require('jsonwebtoken');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(app, passport) {
+  router.get('/', function(req, res) {
+    if (req.isAuthenticated()) {
+      res.redirect("/user");
+    } else {
+      res.render('index', { title: 'Express' });
+    }
+  });
 
-module.exports = router;
+  app.post('/login', function(req, res, next) {
+
+    passport.authenticate('local-login', function(err, user, info) {
+      if (err)
+        return next(err);
+      if (!user) {
+        return res.send({
+          login: false
+        });
+      }
+      req.login(user, function(err) {
+        if (err)
+          return next(err);
+
+        var token = jwt.sign({
+          name: user.id
+        }, "apple-pi-fusion", {
+          expiresInMinutes: 24 * 60
+        });
+
+        return res.send({
+          login: true,
+          token: token
+        });
+      });
+    })(req, res, next);
+
+  });
+
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.get("/user", isAuthenticated, function(req, res) {
+    res.render("index",{
+
+    });
+  });
+
+  app.use('/', routes);
+
+  function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect("/"); // send the user to the landing page if he is not logged in...
+    }
+  }
+};
