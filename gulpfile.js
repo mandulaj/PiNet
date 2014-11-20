2b3aeac196fa0b8950f64b51e7321e7bf1d688d2
Jakub Mandula <jakub.aludnam@gmail.com>
2014-11-20 17:55:01 +0100

    beautify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,38 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-    nodemon = require('gulp-nodemon'),
-    minifyCSS = require('gulp-minify-css'),
-    less = require('gulp-less'),
-    prefix = require('gulp-autoprefixer');
+  uglify = require('gulp-uglify'),
+  nodemon = require('gulp-nodemon'),
+  minifyCSS = require('gulp-minify-css'),
+  less = require('gulp-less'),
+  prefix = require('gulp-autoprefixer');
 
 
 var paths = {
-    images: 'src/images/*',
-    scripts: 'src/js/*.js',
-    less: 'src/less/*.less'
+  images: 'src/images/*',
+  scripts: 'src/js/*.js',
+  less: 'src/less/*.less'
 };
 
 gulp.task('scripts', function() {
-    return gulp.src(paths.scripts)
-      //.pipe(uglify())
-      .pipe(gulp.dest('public/js'));
+  return gulp.src(paths.scripts)
+    //.pipe(uglify())
+    .pipe(gulp.dest('public/js'));
 });
 
-
 gulp.task('images', function() {
- return gulp.src(paths.images)
+  return gulp.src(paths.images)
     .pipe(gulp.dest('public/images'));
 });
 
-gulp.task('less',function() {
-    return gulp.src(paths.less)
-        .pipe(less({
-            keepSpecialComments: 0,
-        }))
-        .pipe(prefix(["last 2 version", "> 1%", "ie 8", "ie 7", "Opera"], { cascade: true }))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('public/css'));
+gulp.task('less', function() {
+  return gulp.src(paths.less)
+    .pipe(less({
+      keepSpecialComments: 0,
+    }))
+    .pipe(prefix(["last 2 version", "> 1%", "ie 8", "ie 7", "Opera"], {
+      cascade: true
+    }))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('watch', function() {
@@ -40,14 +41,18 @@ gulp.task('watch', function() {
   gulp.watch(paths.less, ['less']);
 });
 
-gulp.task('develop', function () {
-  nodemon({ script: 'bin/www', ext: 'html js', ignore: ['src/**','public/**'] })
-    .on('restart', function () {
+gulp.task('develop', function() {
+  nodemon({
+      script: 'bin/www',
+      ext: 'html js',
+      ignore: ['src/**', 'public/**']
+    })
+    .on('restart', function() {
       console.log('restarted!');
     });
 });
 
 
-gulp.task('default', ['scripts', 'images', 'watch','less','develop'], function() {
+gulp.task('default', ['scripts', 'images', 'watch', 'less', 'develop'], function() {
   console.log("Gulp is starting...");
 });
