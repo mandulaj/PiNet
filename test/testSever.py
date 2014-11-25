d5987b9eab4a18b67898da17e016f56e796a294d
Jakub Mandula <jakub.aludnam@gmail.com>
2014-11-25 23:44:00 +0100

    add a testing python server

---
+++ test/testSever.py
@@ -0,0 +1,13 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(("127.0.0.1", 8800))
+sock.listen(1)
+
+while True:
+    (clientsocket, address) = sock.accept()
+    while 1:
+        data = clientsocket.recv(8192)
+        print data
+
+
