d5e6f1a451fb5a1a9eac4cff0ac14ed9464431d3
Jakub Mandula <jakub.aludnam@gmail.com>
2014-11-28 15:15:55 +0000

    clean up Networking Controller

--- driver/NetworkDriver.py
+++ driver/NetworkDriver.py
@@ -31,55 +31,37 @@ DEBUG = False  # If true prints messages marked as 'debug' to console
 class NetworkDriver():
     "Network driver responsible for receiving and parsing external data and forwarding them to the robot"
 
-    def __init__(self, pinArray, host, port):
-        # TODO: this should be removed - the driver should just forward network traffic
-        self.componentsStatus = {
-            "keys": [0, 0, 0, 0],
-            "cam": "default",
-            "speed": 100,
-            "light": False,
-            "laser": False,
-            "ai": False,
-            "running": True
-        }
-
-        self.Robot = PiControl.PiNet(pinArray)
-        self.serversocket = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)  # sets up the socket
-
-        self.SOCKET_HOST = host
-        self.SOCKET_PORT = port
-
+    def __init__(self, host, port, robot):
+        self.Robot = robot
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # sets up the socket
+        
+        self.running = True
         try:
             # binds the port and host to the socket
-            self.serversocket.bind((self.SOCKET_HOST, self.SOCKET_PORT))
+            self.server.bind((host, port))
+            ## Only except in case of socket errors
         except():  # error handling for the bind
-            # -------------------------------------- Does not work ????????????
-            self.writeLog("Error making Socket!")
-            self.writeLog(traceback.format_exc())
+            print("Error making Socket!")
             sys.exit(1)
         else:
-            self.serversocket.listen(1)
-        self.writeLog("Success in setting up socket")
-        self.writeLog("Server running on: " +
-                      str(self.SOCKET_HOST) +
-                      ":" +
-                      str(self.SOCKET_PORT))
+            self.server.listen(1)
+        print("Success in setting up socket")
+        print("Server running on: " + str(host) + ":" + str(port))
 
     def parseData(self, data):
         "function parsing the json commands and directing them to the robot"
         data = json.loads(data)
 
         if data['message'] == "commands":
-            self.componentsStatus = {
+            # send the data to the robot
+            self.sendCommands({
                 "keys": data['keys'],
                 "cam": data['cam'],
                 "speed": data['speed'],
                 "light": data['light'],
                 "laser": data['laser'],
-                "ai": data['ai'],
-                "running":   self.componentsStatus['running']
-            }
+                "ai": data['ai']
+            })
 
         elif data['message'] == "mission":
             if data['status'] == "start":
@@ -87,31 +69,26 @@ class NetworkDriver():
             elif data['status'] == "stop":
                 self.Robot.stopMission()
 
-    def writeLog(self, message):
-        "debug function for logging warnings"
-        print message
-
-    def direction(self):
+    def direction(self, keys):
         "find the direction from the key configuration"
-        keys = self.componentsStatus["keys"]
 
         if keys == [0, 0, 0, 0]:
-            return "stop"
+            return "STOP"
 
         direction = ""
 
         if keys[0] == 1:
-            direction += "f"
+            direction += "F"
         elif keys[2] == 1:
-            direction += "b"
+            direction += "B"
 
         if keys[1] == 1:
-            direction += "l"
+            direction += "L"
         elif keys[3] == 1:
-            direction += "r"
+            direction += "R"
 
         if direction == "":
-            return "stop"
+            return "STOP"
         else:
             return direction
 
@@ -119,55 +96,59 @@ class NetworkDriver():
     def run(self):
         "main loop for listening on a socket and reading its data input"
         try:
-            # Main loop -------------------------------------------------------
-            while self.componentsStatus["running"]:
-                (clientsocket, address) = self.serversocket.accept()
-                while 1:
-                    data = clientsocket.recv(8192)
-                    self.parseData(data)  # parse it
+            while self.running:
+                (conn, address) = self.server.accept()
+                while True:
+                    data = conn.recv(8192)
 
                     if not data:
                         break
 
-                    direction = self.direction()
-                    speed = self.componentsStatus['speed']
-
-                    if direction == "f":
-                        self.Robot.go(speed, 0)
-                    elif direction == "r":
-                        self.Robot.go(speed, 90)
-                    elif direction == "l":
-                        self.Robot.go(speed, -90)
-                    elif direction == "b":
-                        self.Robot.go(speed, 180)
-                    elif direction == "fl":
-                        self.Robot.go(speed, -45)
-                    elif direction == "fr":
-                        self.Robot.go(speed, 45)
-                    elif direction == "bl":
-                        self.Robot.go(speed, -135)
-                    elif direction == "br":
-                        self.Robot.go(speed, 135)
-                    else:  # direction == "stop" or invalid
-                        self.Robot.stop()
-
-                    self.Robot.setLight(self.componentsStatus['light'])
-                    self.Robot.setLaser(self.componentsStatus['laser'])
-                    self.Robot.changeCam(self.componentsStatus['cam'])
-
+                    self.parseData(data)  # parse it
+                
+                conn.close()
+        
         except KeyboardInterrupt:  # catches Ctrl-C Keyboard-Interrupt
-            self.writeLog("Exiting...\n")
+            print("Exiting...\n")
 
         finally:
             # final clean-up
             self.Robot.closeMe()
-            self.serversocket.shutdown(socket.SHUT_RDWR)
-            self.serversocket.close()
-            self.writeLog("Exiting...")
+            self.server.shutdown(socket.SHUT_RDWR)
+            self.server.close()
+            print("Exiting...")
             sys.exit(0)
+
+    def sendCommands(self, commands):
+        "write commands to the underlying robot"
+
+        direction = self.direction(commands['keys'])
+        speed = commands['speed']
+
+        if direction == "F":
+            self.Robot.go(speed, 0)
+        elif direction == "R":
+            self.Robot.go(speed, 90)
+        elif direction == "L":
+            self.Robot.go(speed, -90)
+        elif direction == "B":
+            self.Robot.go(speed, 180)
+        elif direction == "FL":
+            self.Robot.go(speed, -45)
+        elif direction == "FR":
+            self.Robot.go(speed, 45)
+        elif direction == "BL":
+            self.Robot.go(speed, -135)
+        elif direction == "BR":
+            self.Robot.go(speed, 135)
+        else:  # direction == "stop" or invalid
+            self.Robot.stop()
+    
+        self.Robot.setLight(commands['light'])
+        self.Robot.setLaser(commands['laser'])
+        self.Robot.changeCam(commands['cam'])
+
 
 
 if __name__ == "__main__":
-    # TODO: this call will move to the main file
-    network = NetworkDriver(PINS, SOCKET_HOST, SOCKET_PORT)
-    network.run()
+    pass
