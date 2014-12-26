4130e5ecef3525d2de5f2ac6df4ff40e38af0dfb
Jakub Mandula <jakub.aludnam@gmail.com>
2014-12-26 23:38:25 +0100

    add tests for error handeling in dbReader

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 var expect = require("expect.js");
 var dbReader = require("../lib/dbReader.js")
 
-var sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require('sqlite3')
 var database = new sqlite3.Database(":memory:");
+var errorTestdatabase = new sqlite3.Database(":memory:");
 require("../config/db.js")(database);
 database.serialize();
+errorTestdatabase.serialize();
 populateDB(database);
 
 
@@ -221,6 +223,7 @@ describe("ConfigUtil", function(){
 describe("DB Wrapper", function(){
 
   var db = new dbReader(database);
+  var errdb = new dbReader(errorTestdatabase);
 
   describe("#constructor", function(){
     it("should return an object", function(){
@@ -263,6 +266,14 @@ describe("DB Wrapper", function(){
         done();
       });
     });
+    it("should return an error when one happens", function(done){
+      errdb.findById(999999, function(err, user){
+        if (!err) return done(new Error("Has no error but expected one."));
+        expect(user).to.be(null);
+        expect(err).to.be.an(Error);
+        done();
+      });
+    });
   });
   describe("#getIdFromUsername", function(){
     it("should return the id for usernames that exist", function(done){
@@ -279,7 +290,197 @@ describe("DB Wrapper", function(){
         done();
       });
     });
+    it("should return an error when one happens", function(done){
+      errdb.getIdFromUsername("username_does_not_ever_exist", function(err, id){
+        if (!err) return done(new Error("Has no error but expected one."));
+        expect(id).to.be(null);
+        expect(err).to.be.an(Error);
+        done();
+      });
+    });
   });
+  describe("#doesExist", function(){
+    it("should return true if username exists", function(done){
+      db.doesExist("root", function(err, exists){
+        expect(err).to.be(null);
+        expect(exists).to.be(true);
+        done();
+      });
+    });
+    it("should return false if username does not exist", function(done){
+      db.doesExist("username_does_not_ever_exist", function(err, exists){
+        expect(err).to.be(null);
+        expect(exists).to.be(false);
+        done();
+      });
+    });
+    it("should return error when one occurs", function(done){
+      errdb.doesExist("root", function(err, exists){
+        if (!err) return done(new Error("Has no error but expected one."));
+        expect(exists).to.be(null);
+        expect(err).to.be.an(Error);
+        done();
+      });
+    });
+  });
+  describe("#createNewUser", function(){
+    it("should create a new user in the database", function(done){
+      db.createNewUser({
+        username: "newUser123",
+        password: "password",
+        access: 1
+      }, function(err, created){
+        if (err) done(err);
+        expect(created).to.be(true);
+        db.doesExist("newUser123", function(err, exists){
+          if (err) done(err);
+          expect(exists).to.be(true);
+          done();
+        });
+      });
+    });
+    it("should error creating a new user if the username is taken", function(done){
+      db.createNewUser({
+        username: "newUser123",
+        password: "password",
+        access: 1
+      }, function(err, created){
+        expect(created).to.be(false);
+        expect(err).to.be.an(Error);
+        expect(err.code).to.be("SQLITE_CONSTRAINT");
+        done();
+      });
+    });
+    it("should pass an error when one happens", function(done){
+      errdb.createNewUser({
+        username: "newUser123",
+        password: "password",
+        access: 1
+      }, function(err, created){
+        if (!err) return done(new Error("Has no error but expected one."));
+        expect(created).to.be(false);
+        expect(err).to.be.an(Error);
+        done();
+      });
+    });
+  });
+  describe("#verify", function(){
+    it("should return true if password matches the hash", function(done){
+      db.verify('$2a$10$QxVaZEiDyKzlQFUSWT3xDuii.WU7Kxj8h7cDaf704XhZ3ZenslpH6', '123456', function(err, match){
+        expect(err).to.be(null);
+        expect(match).to.be(true);
+        done();
+      });
+    });
+    it("should return false if password does not match the hash", function(done){
+      db.verify('$2a$10$QxVaZEiDyKzlQFUSWT3xDuii.WU7Kxj8h7cDaf704XhZ3ZenslpH6', 'wrongPassword', function(err, match){
+        expect(err).to.be(null);
+        expect(match).to.be(false);
+        done();
+      });
+    });
+    it("should return an error when one happens", function(done){
+      db.verify(123, '123456', function(err, match){
+        if (!err) return done(new Error("Has no error but expected one."));
+        expect(err).to.be.an(Error);
+        expect(match).to.be(false);
+        done();
+      });
+    });
+  });
+  describe("#verifyUser", function(){
+    it("should return true if the password matches the user record", function(done){
+      db.verifyUser(1, "123456", function(err, match){
+        expect(err).to.be(null);
+        expect(match).to.be(true);
+        done();
+      });
+    });
+    it("should return false if the password does not match the user record", function(done){
+      db.verifyUser(2, "wrongPassword", function(err, match){
+        expect(err).to.be(null);
+        expect(match).to.be(false);
+        done();
+      });
+    });
+    it("should return an error if it happens", function(done){
+      errdb.verifyUser(1, "123456", function(err, match){
+        if (!err) return done(new Error("Has no error but expected one."));
+        expect(err).to.be.an(Error);
+        expect(match).to.be(false);
+        done();
+      });
+    });
+  });
+  describe("#updateLogin", function(){
+    it("should set the lastLogin to the current time", function(done) {
+      db.findById(1, function(err, user){
+        if (err) return done(err);
+        var oldTime = parseInt(user.lastLogin);
+        var actualTime = Date.now();
+        db.updateLogin(1, function(err){
+          expect(err).to.be(null);
+          db.findById(1, function(err, user){
+            if (err) return done(err);
+            var newTime = parseInt(user.lastLogin);
+            expect(oldTime).to.not.eql(newTime);
+            expect(Math.abs(newTime - actualTime)).to.be.below(10);
+            done();
+          });
+        });
+      });
+    });
+    it("should return an error when one happens", function(done){
+      errdb.updateLogin(1, function(err){
+        if (!err) return done(new Error("Has no error but expected one."));
+        expect(err).to.be.an(Error);
+        done();
+      });
+    });
+  });
+  describe("#changePassword", function(){
+    it("should update the password to the new password", function(done){
+      db.changePassword(1, "123456", "newPassword", function(err, success){
+        expect(err).to.be(null);
+        expect(success).to.be(true);
+        db.verifyUser(1, "newPassword", function(err, match){
+          expect(err).to.be(null);
+          expect(match).to.be(true);
+          done();
+        });
+      });
+    });
+    it("should refuse to update if old password is wrong", function(done){
+      db.changePassword(1, "123456", "newPassword", function(err, success){
+        expect(err).to.be(null);
+        expect(success).to.be(false);
+        db.verifyUser(1, "newPassword", function(err, match){
+          expect(err).to.be(null);
+          expect(match).to.be(true);
+          done();
+        });
+      });
+    });
+    it("should return an error when one happens", function(done) {
+      errdb.changePassword(1, "123456", "newPassword", function(err, success){
+        if (!err) return done(new Error("Has no error but expected one."));
+        expect(err).to.be.an(Error);
+        expect(success).to.be(false);
+        done();
+      });
+    });
+  });
+  describe("#reportFailedLogin", function(){});
+  describe("#getAccessStatus", function(){});
+  describe("#isAdmin", function(){});
+  describe("#isIpBlocked", function(){});
+  describe("#updateAdminPower", function(){});
+  describe("#checkFormData", function(){});
+  describe("#addSocket", function(){});
+  describe("#removeSocket", function(){});
+  describe("#isSocketBanned", function(){});
+  describe("#socketUserId", function(){});
+
 });
 
 
