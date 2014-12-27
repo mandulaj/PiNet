cef4dcac6c888f10067e706537cee73a8205cf18
Jakub Mandula <jakub.aludnam@gmail.com>
2014-12-27 18:59:56 +0100

    put test utils into a separate file

--- test/dbReader.test.js
+++ test/dbReader.test.js
@@ -1,5 +1,9 @@
 var expect = require("expect.js");
+var populateDB = require("./lib/testUtil.js").populateDB;
+
 var dbReader = require("../lib/dbReader.js")
+
+
 
 var sqlite3 = require('sqlite3')
 var database = new sqlite3.Database(":memory:");
@@ -652,14 +656,3 @@ describe("DB Wrapper", function(){
   });
 });
 
-
-function populateDB(db) {
-  db.run("INSERT INTO users (id, username, password, access, lastLogin) VALUES ((?), (?), (?), (?), (?))",  1, "root", '$2a$10$QxVaZEiDyKzlQFUSWT3xDuii.WU7Kxj8h7cDaf704XhZ3ZenslpH6', 5, Date.now());
-  db.run("INSERT INTO users (id, username, password, access, lastLogin) VALUES ((?), (?), (?), (?), (?))",  2, "user1", '$2a$10$QxVaZEiDyKzlQFUSWT3xDuii.WU7Kxj8h7cDaf704XhZ3ZenslpH6', 0, Date.now());
-  db.run("INSERT INTO users (id, username, password, access, lastLogin) VALUES ((?), (?), (?), (?), (?))",  3, "user2", '$2a$10$QxVaZEiDyKzlQFUSWT3xDuii.WU7Kxj8h7cDaf704XhZ3ZenslpH6', 2, Date.now());
-  db.run("INSERT INTO users (id, username, password, access, lastLogin) VALUES ((?), (?), (?), (?), (?))",  4, "user3", '$2a$10$QxVaZEiDyKzlQFUSWT3xDuii.WU7Kxj8h7cDaf704XhZ3ZenslpH6', 1, Date.now());
-  db.run("INSERT INTO users (id, username, password, access, lastLogin, banned) VALUES ((?), (?), (?), (?), (?), (?))",  5, "user4", '$2a$10$QxVaZEiDyKzlQFUSWT3xDuii.WU7Kxj8h7cDaf704XhZ3ZenslpH6', 1, Date.now(), 1);
-
-  db.run("INSERT INTO logins (id, ip, accessed, banned) VALUES ((?), (?), (?), (?))",  1, "555.555.555.555", 45, 1);
-  db.run("INSERT INTO logins (id, ip, accessed, threat, lastDate) VALUES ((?), (?), (?), (?), (?))",  2, "555.555.555.444", 59, 50, Date.now() - 4000);
-}
