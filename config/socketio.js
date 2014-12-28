4ba42aee6aef2c086ae0c4e31e3c0e0baa7a57bc
Jakub Mandula <jakub.aludnam@gmail.com>
2014-12-28 22:50:15 +0100

    check connecting sockets if not banned

--- config/socketio.js
+++ config/socketio.js
@@ -8,7 +8,7 @@ var socketioJwt = require('socketio-jwt'),
 module.exports = function(socket, database, config){
   var db = new DB(database);
 
-  // Admin only...
+  // Only allow admin connection
   var checkSudo = SocketRouter();
   checkSudo.on('*', function(socket, args, next) {
     db.isSocketSudo(socket.id, function(err, sudo){
@@ -22,6 +22,7 @@ module.exports = function(socket, database, config){
   // Routing
   // *****************************************************
 
+  // Authenticate the socket
   socket.use(socketioJwt.authorize({
     secret: config.secrets.jwt,
     handshake: true
@@ -30,34 +31,34 @@ module.exports = function(socket, database, config){
   // All defined routes, See above
   socket.of("/admin").use(checkSudo);
 
-  // Test if socket is banned
+  // Test any socket if the owner is banned, add the socket to the database + inform the admin
   socket.on("connection", function(sock) {
-    db.isSocketBanned(sock.id, function(err, banned){
+    var id = sock.decoded_token.id;
+    db.isUserBanned(id, function(err, banned){
       if (err) return sock.emit("error", err);
       if (banned) {
-        console.log("banned", sock.id)
         return sock.disconnect();
       } else {
-        var id = sock.decoded_token.id;
         db.addSocket(id, sock.id, function(err){
-          console.log(err)
-          if (err) return sock.destroy();
-
+          if (err) return sock.emit("error");
           // Report this to any admin
-          db.listSockets(function(err, sockets){
-            if (err) sock.emit("error");
-            socket.of("/admin").emit("socketList", {
-              sockets: sockets
-            });
+          db.dataForAdmin(function(err, data){
+            if (err) return sock.emit("error");
+            socket.of("/admin").emit("list", data);
           });
         });
       }
     });
 
+    // remove any disconnecting socket from the database + inform the admin about the change
     sock.on("disconnect", function(){
       db.removeSocket(sock.id, function(err){
-        console.log("removed", sock.id);
-        if (err) console.error(err) // TODO: handle errors
+        if (err) console.error(err); // TODO: handle errors
+        // Update the user about the situation
+        db.dataForAdmin(function(err, data){
+          if (err) return sock.emit("error");
+          socket.of("/admin").emit("list", data);
+        });
       });
     });
   });
@@ -69,4 +70,5 @@ function handleErrors(err, socket, args, next) {
     socket.emit("error");
     // TODO: handle errors
     //next();
-  }
+}
+
